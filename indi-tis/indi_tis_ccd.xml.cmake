322d864bed87f4fe1fbb646fba46c287c20fd9e8
emktubs <eriklein@tu-braunschweig.de>
2019-09-25 11:53:21 +0200

    initial commit tis driver, yet basically generic ccd

---
+++ indi-tis/indi_tis_ccd.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="CCDs">
+        <device label="TIS CCD" mdpd="true">
+                <driver name="TIS CCD">indi_generic_ccd</driver>
+                <version>@TIS_VERSION_MAJOR@.@TIS_VERSION_MINOR@</version>
+	</device>
+</devGroup>
+</driversList>
